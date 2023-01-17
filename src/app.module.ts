0c3e33978dda870e7ea2d3b59b522467259f0524
andersonnaz <anderson_1004@hotmail.com>
2023-01-17 15:07:12 -0300

    build: :art: create service, entities and dto's of tasks, tags and users

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { TasksController } from './tasks/tasks.controller';
 import { UsersController } from './users/users.controller';
 import { TagsController } from './tags/tags.controller';
+import { TasksService } from './tasks/tasks.service';
+import { UsersService } from './users/users.service';
+import { TagsService } from './tags/tags.service';
 
 @Module({
   imports: [],
   controllers: [AppController, TasksController, UsersController, TagsController],
-  providers: [AppService],
+  providers: [AppService, TasksService, UsersService, TagsService],
 })
 export class AppModule {}
---
+++ src/tasks/entities/task.entity.ts
@@ -0,0 +1,9 @@
+
+export class Task {
+    id: string;
+    title: string;
+    description: string;
+    tags: string[];
+    created_at: Date;
+    deleted_at: Date;
+}
