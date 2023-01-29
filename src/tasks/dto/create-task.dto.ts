56577e88bb2769faa6d7660616f0c4539163164a
andersonnaz <anderson_1004@hotmail.com>
2023-01-29 17:51:33 -0300

    build: :construction_worker: added validation on dto's

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,15 @@
+import { IsString } from "class-validator";
+
 export class CreateTaskDto {
+    @IsString()
     readonly title: string;
+
+    @IsString()
     readonly description: string;
+
+    @IsString()
+    readonly user: string;
+
+    @IsString({each: true})
     readonly tags: string[];
 }
