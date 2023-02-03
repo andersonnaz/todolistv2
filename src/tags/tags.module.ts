45c5a8adae79a522c11b61403cc39b5ec9bfa46d
andersonnaz <anderson_1004@hotmail.com>
2023-02-03 06:51:53 -0300

    build: :construction_worker: config datasource from tags

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TagsController } from './tags.controller';
 import { TagsService } from './tags.service';
+import { tagsProviders } from './tags.providers';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+    imports: [DatabaseModule],
     controllers: [TagsController],
-    providers: [TagsService]
+    providers: [TagsService, ...tagsProviders]
 })
 export class TagsModule {}
